0d118c01602e13ea1fbe3db8c28a510729607438
Aurelien Marc <aurelien.marc@groupecimes.com>
2021-12-06 22:17:58 -0500

    reset du projet 2

--- spring/src/main/java/com/tpam/inviteme/dao/IUserDao.java
+++
@@ -1,9 +0,0 @@
-package com.tpam.inviteme.dao;
-
-import com.tpam.inviteme.beans.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserDao extends JpaRepository<User, Integer> {
-
-
-}
--- spring/src/main/java/com/u8inf422/spring/Application.java
+++ spring/src/main/java/com/u8inf422/spring/Application.java
@@ -1,15 +1,13 @@
-package com.tpam.inviteme;
+package com.u8inf422.spring;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@Slf4j
-public class InvitemeApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(InvitemeApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
