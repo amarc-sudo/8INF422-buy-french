3c8290e170f0dd48a41ddd011bfa2fe40590a436
Aurelien Marc <aurelien.marc@groupecimes.com>
2021-12-08 14:53:25 -0500

    Change : livraison-commande-selec.component.html
    ajout d'une map fonctionelle

--- angulars/src/app/pages/home-livraison/livraison-commande-selec/livraison-commande-selec.component.ts
+++ angulars/src/app/pages/home-livraison/livraison-commande-selec/livraison-commande-selec.component.ts
@@ -1,12 +1,29 @@
 import {Component, Input, OnInit, ViewChild} from '@angular/core';
 import {} from 'googlemaps';
 import {command} from "../../../api/objects/command";
+import {GoogleMap} from "@angular/google-maps";
 @Component({
   selector: 'app-livraison-commande-selec',
   templateUrl: './livraison-commande-selec.component.html',
   styleUrls: ['./livraison-commande-selec.component.css']
 })
 export class LivraisonCommandeSelecComponent implements OnInit {
+
+
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  directionsService = new google.maps.DirectionsService();
+  marker = {
+    position: { lat: 48.42945753317865, lng: -71.05447288192586 },
+  }
+  marker2 = {
+    position: { lat: 48.420082000849604,  lng: -71.05320127515539 },
+  }
+  mapOptions: google.maps.MapOptions = {
+    center: { lat: 48.4248013000852, lng: -71.05849093405284 },
+    zoom : 14
+  }
+  @ViewChild(GoogleMap) map!: GoogleMap;
+
 
 
   @Input() command : command | undefined;
