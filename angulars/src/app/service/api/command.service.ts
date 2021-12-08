afa6272434c33a8ba12637a9d24b5839543ca952
Aurelien Marc <aurelien.marc@groupecimes.com>
2021-12-07 20:20:11 -0500

    update docker-conpose il compile ENFIN PUTAIN CA FAIT DU BIEN

---
+++ angulars/src/app/service/api/command.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {ParentApiService} from "./parent.api.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {environment} from "../../../environments/environment";
+import {product} from "../../api/objects/product";
+import {user} from "../../api/objects/User";
+import {command} from "../../api/objects/command";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommandService extends ParentApiService {
+
+  constructor( private httpClient: HttpClient) {
+    super();
+  }
+
+  listCommand(users:user): Observable<Array<command>> {
+    let headers: HttpHeaders = new HttpHeaders();
+    headers = headers.append('Accept', 'application/json');
+    return this.httpClient.post<Array<command>>(environment.apiUrl + '/rest/api/Command/getHistory',users);
+  }
+
+}
